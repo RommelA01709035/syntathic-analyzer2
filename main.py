0af4e9f892f20c410300b5496a79921925676bdb
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2026-05-29 12:05:07 -0600

    feature: run all tests

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from Lexer import *
 from Parser import *
-import sys
+
+files = [
+    "test_cases/good/input01.txt",
+    "test_cases/good/input02.txt",
+    "test_cases/bad/input03.txt",
+    "test_cases/bad/input04.txt",
+]
 
 if __name__ == '__main__':
-    parser = Parser("test_cases/good/input02.txt")
-    parser.analize()
+    for filepath in files:
+        print(f"\n--- {filepath} ---")
+        try:
+            parser = Parser(filepath)
+            parser.analize()
+        except Exception as e:
+            print(f"ERROR: {e}")
